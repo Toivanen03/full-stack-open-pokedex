13787b5b76c33a851cac84612b317b50ebe08a8a
Simo Toivanen <posti@simotoivanen.fi>
2025-04-14 08:59:47 +0300

    Päivitetty Playwright-testin konfiguraatiota

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@playwright/test')
 
 module.exports = defineConfig({
   testDir: './e2e-tests',
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:8080',
