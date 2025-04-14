9eef8ddfde4d9536eadc1c4fc7e98adf857fb019
Simo Toivanen <posti@simotoivanen.fi>
2025-04-14 13:43:29 +0300

    Push-testi

--- build_step.sh
+++ build_step.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 echo "Build script"
+
+echo "Installing dependencies"
 npm install
+
+echo "Checking code style"
 npm run eslint
+
+echo "Running tests"
 npm test
+
+echo "Running end-to-end tests"
+npm run test:e2e
+
+echo "Compiling build"
 npm run build
