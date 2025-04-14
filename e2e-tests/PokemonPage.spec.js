b1f79fcc744f9fd2e71445fb523e0398277ce558
Simo Toivanen <posti@simotoivanen.fi>
2025-04-14 09:52:17 +0300

    Lisätty toinen testi

--- e2e-tests/PokemonPage.spec.js
+++ e2e-tests/PokemonPage.spec.js
@@ -6,4 +6,11 @@ test.describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('pokemon page can be opened', async ({ page }) => {
+    await page.goto('')
+    const link = (await page.locator('.list-item').first().innerText()).toLowerCase()
+    await expect(page.getByText(`${link}`)).toBeVisible()
+    await page.goto(`/pokemon/${link}`)
+    await expect(page.getByText('overgrow')).toBeVisible()
+  })
 })
